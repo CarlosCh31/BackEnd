b3df87fed4703b9376d3eb723b64c7a9ca01687e
Remote <ama.esq.alv@gmail.com>
2025-04-29 00:32:42 -0600

    voluntario

--- src/main/java/com/olimpiadas/inscriptionsback/Repositories/VolunteerRepository.java
+++ src/main/java/com/olimpiadas/inscriptionsback/Repositories/VolunteerRepository.java
@@ -4,21 +4,48 @@ import com.olimpiadas.inscriptionsback.Models.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Integer> {
 
-    @Modifying
+
+
     @Transactional
-    @Query(value = "INSERT INTO volunteer (id, identification, name, birthdate, region_id, email, phone_number, nationality) " +
-            "VALUES (:#{#volunteer.id}, :#{#volunteer.identification}, :#{#volunteer.name}, :#{#volunteer.birthdate}, :#{#volunteer.region.id}, " +
-            ":#{#volunteer.email}, :#{#volunteer.phone_number}, :#{#volunteer.nationality})",
+    @Query(value = "SELECT register_volunteer(" +
+            "CAST(:p_id AS VARCHAR), " +
+            "CAST(:p_name AS VARCHAR), " +
+            "CAST(:p_birthdate AS DATE), " +
+            "CAST(:p_region_id AS INT), " +
+            "CAST(:p_email AS VARCHAR), " +
+            "CAST(:p_phone_number AS VARCHAR), " +
+            "CAST(:p_tipo AS VARCHAR), " +
+            "CAST(:p_province AS INT), " +
+            "CAST(:p_canton AS INT), " +
+            "CAST(:p_citizenship AS VARCHAR), " +
+            "CAST(:v_sport_experience AS VARCHAR))",
             nativeQuery = true)
-    void saveVolunteer(Volunteer volunteer);
+    String saveVolunteer(
+            @Param("p_id") String id,
+            @Param("p_name") String name,
+            @Param("p_birthdate") Date birthdate,
+            @Param("p_region_id") Integer regionId,
+            @Param("p_email") String email,
+            @Param("p_phone_number") String phoneNumber,
+            @Param("p_tipo") String tipo,
+            @Param("p_province") Integer provinceId,
+            @Param("p_canton") Integer cantonId,
+            @Param("p_citizenship") String citizenship,
+            @Param("v_sport_experience") String sportExperience
+    );
+
+
+
 
     @Transactional
     @Query(value = "SELECT update_volunteer_sport_experience(:id, :sportExperience)", nativeQuery = true)
--- src/main/java/com/olimpiadas/inscriptionsback/Service/VolunteerServiceImpl.java
+++ src/main/java/com/olimpiadas/inscriptionsback/Service/VolunteerServiceImpl.java
@@ -3,14 +3,17 @@ package com.olimpiadas.inscriptionsback.Service;
 import com.olimpiadas.inscriptionsback.Models.Volunteer;
 import com.olimpiadas.inscriptionsback.Repositories.VolunteerRepository;
 import com.olimpiadas.inscriptionsback.exception.ResourceNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class VolunteerServiceImpl implements VolunteerService {
-
+    @Autowired
     private final VolunteerRepository volunteerRepository;
 
     public VolunteerServiceImpl(VolunteerRepository volunteerRepository) {
@@ -24,7 +27,20 @@ public class VolunteerServiceImpl implements VolunteerService {
 
     @Override
     public void saveVolunteer(Volunteer volunteer) {
-        volunteerRepository.saveVolunteer(volunteer);
+        volunteerRepository.saveVolunteer(
+                volunteer.getId(),
+                volunteer.getName(),
+                volunteer.getBirthdate(),
+                volunteer.getRegion_id().getId(),
+                volunteer.getEmail(),
+                volunteer.getPhone_number(),
+                volunteer.getTipo(),
+                volunteer.getProvince_id().getId(),
+                volunteer.getCanton_id().getId(),
+                volunteer.getCitizenship(),
+                volunteer.getSportExperience()
+        );
+
     }
 
     @Override
